017234342b643752a183d29b0be2bcd9665132b1
weikangYeo <weikang203@gmail.com>
2026-03-14 15:16:59 +0800

    fix NaN issue

--- frontend/src/app/gold-mgmt/gold-mgmt.ts
+++ frontend/src/app/gold-mgmt/gold-mgmt.ts
@@ -1,4 +1,4 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import {GoldTxn} from './gold.model';
 import {GoldService} from './gold.service';
 
@@ -14,10 +14,29 @@ export class GoldMgmt {
   private goldService = inject(GoldService);
   protected goldTransactions = signal<GoldTxn[]>([]);
 
-  ngOnInit(){
-    this.goldService.getAllTransactions().subscribe( data => {
+  // Computed signal are read-only signals that derive their value from other signals.
+  // it is lazy loaded and only calculated when upstream/depended signal done
+  protected totalGrams = computed(() =>
+    this.goldTransactions().reduce((total, txn) =>
+      txn.txnType === 'BUY' ? total + Number(txn.gram) : total - Number(txn.gram), 0
+    )
+  );
+
+  protected totalPurchased = computed(() =>
+    this.goldTransactions().reduce((total, txn) =>
+      txn.txnType === 'BUY' ? total + Number(txn.totalPrice) : total - Number(txn.totalPrice), 0
+    )
+  );
+
+  protected avgPurchasePrice = computed(() => {
+    const gram = this.totalGrams();
+    return gram > 0 ? this.totalPurchased() / gram : 0;
+  });
+
+  ngOnInit() {
+    this.goldService.getAllTransactions().subscribe(data => {
       this.goldTransactions.set(data);
-    })
+    });
   }
 
 }
