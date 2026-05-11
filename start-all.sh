423ee18dcd28b15ed82dc05fd7f433945b36aa39
weikangYeo <weikang203@gmail.com>
2026-05-11 23:27:30 +0800

    add health check, add startup script to start all component at once

---
+++ start-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cleanup() {
+  trap - SIGINT SIGTERM
+  echo "stopping all services"
+  echo "stopping dockers"
+  docker compose -f devops/docker-compose.yml down
+  kill 0
+  exit 0
+}
+trap cleanup SIGINT SIGTERM
+
+docker compose -f devops/docker-compose.yml up --wait
+(cd frontend && ng serve) &
+(cd backend && go run cmd/server/main.go) &
+wait
